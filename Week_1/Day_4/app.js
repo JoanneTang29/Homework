653880fb3cd68aff65da9b84eda70d61a5532632
Joanne Tang <joanne@Joannes-MacBook-Air.local>
2022-04-10 22:46:39 -0400

    day 4 stuff

--- Week_1/Day_4/app.js
+++ Week_1/Day_4/app.js
@@ -1,12 +1,57 @@
 let store = ['nike', 'adidas', 'puma'];
 let shoppingCart = [];
 
-for (let i = 0; i < 3; i++) {
-    shoppingCart[i] = store[i]
+let nikeProduct = {
+    name: "Nike Shoes",
+    price: 80,
+    description: "all white shoes"
 }
 
-shoppingCart[0] = store[0];
+let adidasProduct = {
+    name: "Adidas Shoes",
+    price: 140,
+    description: "all black shoes"
+}
 
-console.log(shoppingCart[0]);
-console.log(shoppingCart[1]);
-console.log(shoppingCart[2]);
+let pumaProduct = {
+    name: "Puma Shoes",
+    price: 100,
+    description: "all white shoes"
+}
+
+let allProducts = [nikeProduct, adidasProduct, pumaProduct]
+
+let totalItemsInCart = document.getElementById('numberHolder').innerHTML;
+
+function addToCart(newValue, brand) {
+console.log(brand)
+// Recognize Item
+newValue = totalItemsInCart++;
+
+console.log(totalItemsInCart);
+
+// Take the ItemName --> addToCart
+if (brand === 'nike') {
+    shoppingCart.push(nikeProduct.name);
+} else if (brand === 'adidas') {
+    shoppingCart.push(adidasProduct.name);
+} else if (brand === 'puma') {
+    shoppingCart.push(pumaProduct.name);
+}
+
+// display new result of cart now that something has been adeded
+document.getElementById('numberHolder').innerHTML = totalItemsInCart;
+console.log(shoppingCart);
+}
+
+
+
+// for (let i = 0; i < 3; i++) {
+//     shoppingCart[i] = store[i]
+// }
+
+// shoppingCart[0] = store[0];
+
+// console.log(shoppingCart[0]);
+// console.log(shoppingCart[1]);
+// console.log(shoppingCart[2]);
