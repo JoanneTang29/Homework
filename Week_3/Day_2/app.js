909c07157afc831a5c93856621186fd81e5359ee
Joanne Tang <joannetangrd@gmail.com>
2022-04-21 22:33:59 -0400

    new hw

---
+++ Week_3/Day_2/app.js
@@ -0,0 +1,15 @@
+
+const colors = ['#f6bd60', '#d90429', '#118ab2', 'white'];
+let counter = 0;
+
+function changeColor(){
+    let gridItem = document.getElementsByClassName("gridItem");
+    for (let i=0; i<gridItem.length; i++) {
+        console.log(gridItem.length)
+    gridItem[i].style.backgroundColor = colors[counter];
+    }
+    counter++;
+    if (counter > 3) {
+        counter = 0;
+    }
+}
