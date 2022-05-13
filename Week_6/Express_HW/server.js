0b930d257d6abb65de79553957a0d2413de2c481
Joanne Tang <joannetangrd@gmail.com>
2022-05-13 14:19:07 -0400

    hw update

---
+++ Week_6/Express_HW/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+let magicResponse = [
+  'It is certain',
+  'It is decidedly so',
+  'Without a doubt',
+  'Yes definitely',
+  'You may rely on it',
+  'As I see it yes',
+  'Most likely',
+  'Outlook good',
+  'Yes',
+  'Signs point to yes',
+  'Reply hazy try again',
+  'Ask again later',
+  'Better not tell you now',
+  'Cannot predict now',
+  'Concentrate and ask again',
+  "Don't count on it",
+  'My reply is no',
+  'My sources say no',
+  'Outlook not so good',
+  'Very doubtful',
+];
+
+app.get('/greeting', (req, res) => {
+  res.send('Hello stranger');
+});
+
+app.get('/greeting/:name', (req, res) => {
+  res.send(`Hello ${req.params['name']}`);
+});
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+  const { total, tipPercentage } = req.params;
+  const tip = total * (tipPercentage / 100);
+  res.send(200, tip);
+});
+
+app.get('/magic/:question', (req, res) => {
+  const randomResponse =
+    magicResponse[Math.floor(Math.random() * magicResponse.length)];
+  res.send(`<h1>${req.params['question']}, ${randomResponse} </h1>`);
+});
+
+app.listen(port, () => {
+  console.log('listening on port', port);
+});
