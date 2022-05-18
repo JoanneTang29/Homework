9e249490800215954eb93ac50c8d16033814106d
Joanne Tang <joannetangrd@gmail.com>
2022-05-17 21:37:21 -0400

    index.jsx view rendered at pokemon route

--- Week_7/pokemon_app/server.js
+++ Week_7/pokemon_app/server.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
 const pokemon = require('./models/pokemon');
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Pokemon App!');
 });
 
-app.get('/pokemon', (req, res) => {
-  res.send(pokemon);
+app.get('/pokemon/', (req, res) => {
+  res.render('Index', { pokemon: pokemon });
 });
 
 app.listen(port, () => {
