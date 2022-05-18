8203f8bb16819c6083a244db13914d7c1e532c49
Joanne Tang <joannetangrd@gmail.com>
2022-05-17 22:02:00 -0400

    I can view all pokemon in browser

--- Week_7/pokemon_app/views/Index.jsx
+++ Week_7/pokemon_app/views/Index.jsx
@@ -5,11 +5,27 @@ const myStyle = {
     backgroundColor: '#000000',
   };
 class Index extends React.Component{
-    render(){
-        return(
-            <h1 style={myStyle}>See All The Pokemon!</h1>
-        )
+   render(){
+    const {pokemon} = this.props;
+    return(
+        <div style={myStyle}>
+            <h1>See All The Pokemon!</h1>
+            <ul>
+                {pokemon.map((pokemon, i) => {
+                    return(
+                        <li>
+                            <text>{capitalize(pokemon.name)}</text>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
     }
+};
+
+function capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 module.exports = Index;
